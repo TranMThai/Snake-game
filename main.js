f73a4dd0285f8a3367b26a349fb3a330b95a5ce3
Trần Minh Thái <thaitranminh564@gmail.com>
2024-04-13 17:25:12 +0700

    Fixed snake's movement and added function eat food, then increase size

--- main.js
+++ main.js
@@ -20,39 +20,40 @@ let snake = [
     }
 ]
 
+//contructor snake
+function addSnake(x,y){
+    this.x = x;
+    this.y = y
+}
+
 arr[snake[snake.length - 1].y][snake[snake.length - 1].x] = 'active';
 
 let snakeMove = 'right'
 
+foodRespawn()
 
 let refresh = setInterval(() => {
 
     move()
 
+    if(ateFood()){
+        foodRespawn()
+    }
+
     refreshGameScreen()
 
-}, 150)
+}, 200)
 
 
 //Snake move
 function move() {
-    if (snake[snake.length - 1].x == 20 || snake[snake.length - 1].x < 0 ||
-        snake[snake.length - 1].y == 20 || snake[snake.length - 1].y < 0) {
+    if (snake[snake.length - 1].x >= 20 || snake[snake.length - 1].x < 0 ||
+        snake[snake.length - 1].y >= 20 || snake[snake.length - 1].y < 0) {
         gameOver()
         return
     }
-    arr[snake[0].y][snake[0].x] = ''
     
-    for (let i = 0 ; i < snake.length ; i++) {
-        arr[snake[i].y][snake[i].x] = '';
-        console.log(arr[snake[i].y][snake[i].x])
-
-        if (i + 1 < snake.length) {
-            snake[i] = snake[i + 1]
-        }
-
-        arr[snake[i].y][snake[i].x] = 'active';
-    }
+    updateSnake()
 
     switch (snakeMove) {
         case 'right': {
@@ -72,10 +73,53 @@ function move() {
             break
         }
     }
-    arr[snake[snake.length - 1].y][snake[snake.length - 1].x] = 'active';
+    
+    updateArr()
 }
 
+//Update snake X Y
+function updateSnake(){
+    for (let i = 0 ; i < snake.length ; i++) {
+        if (i + 1 < snake.length) {
+            snake[i] = {... snake[i+1]}
+        }
+    }
+}
 
+//Update arr
+function updateArr(){
+    for(let y in arr){
+        for(let x in arr[y]){
+            for(let s of snake){
+                if(y==s.y && x==s.x){
+                    arr[y][x]='active'
+                    break
+                }
+                else if(arr[y][x]=='food'){}
+                else{
+                    arr[y][x]=''
+                }
+            }
+        }
+    }
+}
+
+// Check if the snake has eaten food
+function ateFood(){
+    if(snake[snake.length-1].x==foodX && snake[snake.length-1].y==foodY){
+        snake.unshift(new addSnake(snake[0].x,snake[0].y))
+        return true
+    }
+    return false
+}
+
+// Food respawn
+function foodRespawn(){
+    foodX = Math.floor(Math.random() * 20);
+    foodY = Math.floor(Math.random()*20);
+
+    arr[foodY][foodX] = 'food';
+}
 // Refresh the game screen every 200 milliseconds
 function refreshGameScreen() {
     let html = ``
@@ -106,7 +150,7 @@ window.addEventListener('keypress', function (event) {
     }
 })
 
-
+//game over
 function gameOver() {
     clearInterval(refresh)
     alert("game over")
